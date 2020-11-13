5f51438ad75109ae8ce695b6fe89ebf4536c78e5
ytz4178 <yintianze49@gmail.com>
2020-11-13 16:03:25 +0800

    mobile display fix

--- js/app.js
+++ js/app.js
@@ -7,9 +7,9 @@ function PopWindowClose(){
     $(".overlay").hide();
 }
 
-AddCard(){
+function AddCard(){
     let main = $("#main");
-    main.append("");
+    main.append('<div class="card-header"><h2>最近浏览</h2></div><div class="card-control"><button class="all-btn control-btn clear-btn"><img src="img/clear_btn1.svg" /></button></div><div class="card-content"><ul class="book-list"><li><div class="book-cover"></div></li></ul></div></div>');
 }
 
 $(".avator-ul>li:first-child>a").click(PopWindowOpen);
