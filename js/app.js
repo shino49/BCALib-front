0eeef8d6e01a353bee17b2c7c21889641b44df95
ytz4178 <yintianze49@gmail.com>
2020-11-16 00:13:44 +0800

    small func add2

--- js/app.js
+++ js/app.js
@@ -10,15 +10,18 @@ function PopWindowClose(){
 function AddCard(){
     let main = $("#main");
     let cardName = prompt("Enter the card name");
-    main.append('<div class="card" onclick="RemoveCard(this);"><div class="card-header"><h2>'+cardName+'</h2></div><div class="card-control"><button class="all-btn control-btn clear-btn"><img src="img/clear_btn1.svg" /></button></div><div class="card-content"><ul class="book-list"><li><div class="book-cover"></div></li></ul></div></div></div');
+    main.append('<div class="card"><div class="card-header"><h2>' + cardName +'</h2></div><div class="card-control"><button class="all-btn control-btn clear-btn" onclick="RemoveCard(this);"><img src="img/clear_btn1.svg" /></button></div><div class="card-content"><ul class="book-list"><li><div class="book-cover"></div></li></ul></div></div></div');
 }
 function RemoveCard(thisCard){
-    thisCard.parentNode.removeChild(thisCard);
+    let card = thisCard.parentNode.parentNode;
+    card.parentNode.removeChild(card);
 }
 
-$(".avator-ul>li:first-child>a").click(PopWindowOpen);
-$(".overlay").click(PopWindowClose);
-// $(".card-control .clear-btn").click((obj) => {
-//     let card = obj.target.parentNode.parentNode.parentNode;
-//     card.parentNode.removeChild(card);
-// });
+$(function() {
+    $(".avator-ul>li:first-child>a").click(PopWindowOpen);
+    $(".overlay").click(PopWindowClose);
+    // $(".card-control .clear-btn").click((obj) => {
+    //     let card = obj.target.parentNode.parentNode.parentNode;
+    //     card.parentNode.removeChild(card);
+    // });
+});
