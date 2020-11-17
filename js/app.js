f5e9897d33a2d47f1e194922483158d09bfdf8bb
ytz4178 <yintianze49@gmail.com>
2020-11-17 23:07:48 +0800

    anime fix

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
 function PopWindowOpen() {
-    $(".overlay").show();
-    $(".pop-window").show();
+    $(".overlay").show(200);
+    $(".pop-window").show(300);
 }
 function PopWindowClose(){
-    $(".pop-window").hide();
-    $(".overlay").hide();
+    $(".pop-window").hide(200);
+    $(".overlay").hide(300);
 }
 
 function AddCard(){
     let main = $("#main");
     let cardName = prompt("Enter the card name");
     main.append('<div class="card"><div class="card-header"><h2>' + cardName +'</h2></div><div class="card-control"><button class="all-btn control-btn clear-btn" onclick="RemoveCard(this);"><img src="img/clear_btn1.svg" /></button></div><div class="card-content"><ul class="book-list"><li><div class="book-cover"></div></li></ul></div></div></div');
+    console.log(main.children().first());
 }
 function RemoveCard(thisCard){
-    let card = thisCard.parentNode.parentNode;
-    card.parentNode.removeChild(card);
+    let card = $(thisCard.parentNode.parentNode);
+    card.fadeOut(300,()=>{card.remove();});
 }
 
 $(function() {
@@ -25,3 +26,7 @@ $(function() {
     //     card.parentNode.removeChild(card);
     // });
 });
+
+function UpdateCard(cardObj) {
+
+}
