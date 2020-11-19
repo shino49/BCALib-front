e42d5254d3b4361a24caf551036c38e868a2eff1
ytz4178 <yintianze49@gmail.com>
2020-11-19 16:59:30 +0800

    side card add

--- js/app.js
+++ js/app.js
@@ -7,11 +7,18 @@ function PopWindowClose(){
     $(".overlay").hide(300);
 }
 
-function AddCard(){
-    let main = $("#main");
-    let cardName = prompt("Enter the card name");
-    main.append('<div class="card"><div class="card-header"><h2>' + cardName +'</h2></div><div class="card-control"><button class="all-btn control-btn clear-btn" onclick="RemoveCard(this);"><img src="img/clear_btn1.svg" /></button></div><div class="card-content"><ul class="book-list"><li><div class="book-cover"></div></li></ul></div></div></div');
-    console.log(main.children().first());
+function AddCard(area_name){
+    if(area_name=="side"){
+        let area = $("#side-card-area");
+        let cardName = prompt("Enter the card name");
+        area.append('<div class="side-card"><div class="card-header"><h2>' + cardName +'</h2></div><div class="card-control"><button class="all-btn control-btn clear-btn" onclick="RemoveCard(this);"><img src="img/clear_btn1.svg" /></button></div><div class="card-content"><ul class="book-list-small"><li><div class="book-cover"></div></li></ul></div></div></div');
+    }
+    else if(area_name=="main"){
+        let main = $("#main-card-area");
+        let cardName = prompt("Enter the card name");
+        main.append('<div class="card"><div class="card-header"><h2>' + cardName +'</h2></div><div class="card-control"><button class="all-btn control-btn clear-btn" onclick="RemoveCard(this);"><img src="img/clear_btn1.svg" /></button></div><div class="card-content"><ul class="book-list-big"><li><div class="book-cover"></div></li></ul></div></div></div');
+    }
+    
 }
 function RemoveCard(thisCard){
     let card = $(thisCard.parentNode.parentNode);
